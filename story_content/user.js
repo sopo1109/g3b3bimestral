3c4c803bb13aa83a538ecdaecd2b52e654c4de46
sopo1109 <manuel.sopo@gmail.com>
2020-08-20 15:22:04 -0500

    nuevo

---
+++ story_content/user.js
@@ -0,0 +1,139 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5t4HTLDpgZ4":
+        Script1();
+        break;
+      case "61zlD5rCDb6":
+        Script2();
+        break;
+      case "62vJmjXfpic":
+        Script3();
+        break;
+      case "5kUQJhrsEoe":
+        Script4();
+        break;
+      case "5YM6TRwGrj2":
+        Script5();
+        break;
+      case "6r7pO4dZLkY":
+        Script6();
+        break;
+      case "5WFsThZ4STy":
+        Script7();
+        break;
+      case "5nw3W9dRj7g":
+        Script8();
+        break;
+  }
+}
+
+function Script1()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1597952836&chart=MjQyfDE1MjJ8ZGU4ZDAwNzYwZmMwNTVhM2JiYWY0NTFmMmVmMThhZDM";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var nombre = player.GetVar("nombre");
+var apellido = player.GetVar("apellido");
+var curso = player.GetVar("curso");
+var uno = player.GetVar("Ejercicio1");
+var dos = player.GetVar("Ejercicio2");
+var tres = player.GetVar("Ejercicio3");
+var cuatro = player.GetVar("Ejercicio4");
+var cinco = player.GetVar("Ejercicio5");
+var seis = player.GetVar("Ejercicio6");
+var siete = player.GetVar("Ejercicio7");
+var ocho = player.GetVar("Ejercicio8");
+var nueve = player.GetVar("Ejercicio9");
+var diez = player.GetVar("Ejercicio10");
+var def = player.GetVar("Definitiva");
+var informacion = curso.concat(";",apellido," ",nombre,";",uno,";",dos,";",tres,";",cuatro,";",cinco,";",seis,";",siete,";",ocho,";",nueve,";",diez,";",def);
+CloudSet('info', informacion);
+}
+
+function Script3()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1597953016&chart=MjQzfDE1MjN8Y2ZjNzAzMjA1ODk2OWVmNjg0NGI3NThhMjFiMmJiYzY";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var nombre = player.GetVar("nombre");
+var apellido = player.GetVar("apellido");
+var curso = player.GetVar("curso");
+var uno = player.GetVar("Ejercicio1");
+var dos = player.GetVar("Ejercicio2");
+var tres = player.GetVar("Ejercicio3");
+var cuatro = player.GetVar("Ejercicio4");
+var cinco = player.GetVar("Ejercicio5");
+var seis = player.GetVar("Ejercicio6");
+var siete = player.GetVar("Ejercicio7");
+var ocho = player.GetVar("Ejercicio8");
+var nueve = player.GetVar("Ejercicio9");
+var diez = player.GetVar("Ejercicio10");
+var def = player.GetVar("Definitiva");
+var informacion = curso.concat(";",apellido," ",nombre,";",uno,";",dos,";",tres,";",cuatro,";",cinco,";",seis,";",siete,";",ocho,";",nueve,";",diez,";",def);
+CloudSet('info', informacion);
+}
+
+function Script5()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1597953101&chart=MjQ0fDE1MjR8YTJiNTA0NTE4NjcyOTI4OWRlNTZhNDAxMDRiYTVhMDE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var nombre = player.GetVar("nombre");
+var apellido = player.GetVar("apellido");
+var curso = player.GetVar("curso");
+var uno = player.GetVar("Ejercicio1");
+var dos = player.GetVar("Ejercicio2");
+var tres = player.GetVar("Ejercicio3");
+var cuatro = player.GetVar("Ejercicio4");
+var cinco = player.GetVar("Ejercicio5");
+var seis = player.GetVar("Ejercicio6");
+var siete = player.GetVar("Ejercicio7");
+var ocho = player.GetVar("Ejercicio8");
+var nueve = player.GetVar("Ejercicio9");
+var diez = player.GetVar("Ejercicio10");
+var def = player.GetVar("Definitiva");
+var informacion = curso.concat(";",apellido," ",nombre,";",uno,";",dos,";",tres,";",cuatro,";",cinco,";",seis,";",siete,";",ocho,";",nueve,";",diez,";",def);
+CloudSet('info', informacion);
+}
+
+function Script7()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1597698258&chart=MjQ1fDE1MjV8NWNjZGM2NDRlMzdhOTNlMWNkODA5MGQwNDYwZDU3Mzg";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var nombre = player.GetVar("nombre");
+var apellido = player.GetVar("apellido");
+var curso = player.GetVar("curso");
+var uno = player.GetVar("Ejercicio1");
+var dos = player.GetVar("Ejercicio2");
+var tres = player.GetVar("Ejercicio3");
+var cuatro = player.GetVar("Ejercicio4");
+var cinco = player.GetVar("Ejercicio5");
+var seis = player.GetVar("Ejercicio6");
+var siete = player.GetVar("Ejercicio7");
+var ocho = player.GetVar("Ejercicio8");
+var nueve = player.GetVar("Ejercicio9");
+var diez = player.GetVar("Ejercicio10");
+var def = player.GetVar("Definitiva");
+var informacion = curso.concat(";",apellido," ",nombre,";",uno,";",dos,";",tres,";",cuatro,";",cinco,";",seis,";",siete,";",ocho,";",nueve,";",diez,";",def);
+CloudSet('info', informacion);
+}
+
